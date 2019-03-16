bab8621bf29ec1b43cb38e9cbfa511e5f97299b1
azizurrahmaan <azizurrahmaan1997@gmail.com>
2019-03-17 03:08:09 +0500

    Added popper.

---
+++ application/views/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Bootstrap Example</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap/css/bootstrap.min.css');?>">
+	<script src="<?php echo base_url('assets/js/jquery.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/js/popper.min.js');?>"></script>
+	<script src="<?php echo base_url('assets/css/bootstrap/js/bootstrap.min.js');?>"></script>
+</head>
+<body>
---
+++ application/views/login.php
@@ -0,0 +1,29 @@
+<div class="container">
+
+	<div class="row" style="margin-top: 10%;">
+		<div class="col-xl"></div>
+
+		<div class="col-xl">
+			<h3>Login to National Tutors</h3>
+			<form action="<?php site_url('home');?>">
+				<div class="form-group">
+					<label for="email">Email address:</label>
+					<input type="email" class="form-control" id="email">
+				</div>
+				<div class="form-group">
+					<label for="pwd">Password:</label>
+					<input type="password" class="form-control" id="pwd">
+				</div>
+				<div class="form-group form-check">
+					<label class="form-check-label">
+						<input class="form-check-input" type="checkbox"> Remember me
+					</label>
+				</div>
+				<button type="submit" class="btn btn-block btn-primary">Submit</button>
+			</form>		
+		</div>
+
+		<div class="col-xl"></div>
+	</div>
+</div>
+
