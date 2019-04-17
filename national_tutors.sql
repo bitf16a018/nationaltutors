a1681a7025fc794f15281e0485ebbdb471e92b46
Arslan Ahmad <bitf16a018@pucit.edu.pk>
2019-04-17 18:15:32 +0500

    databases updated

--- national_tutors.sql
+++ national_tutors.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 16, 2019 at 06:32 PM
+-- Generation Time: Apr 17, 2019 at 03:12 PM
 -- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.2
+-- PHP Version: 7.3.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -42,6 +42,146 @@ CREATE TABLE `administrator` (
 INSERT INTO `administrator` (`id`, `name`, `email`, `password`) VALUES
 (1, 'admin', 'a@a.a', 'abc');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `area`
+--
+
+CREATE TABLE `area` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `area`
+--
+
+INSERT INTO `area` (`id`, `name`) VALUES
+(1, 'Model Town'),
+(2, 'Johar Town'),
+(3, 'Bahria Town'),
+(4, 'Gajju Ki Maata'),
+(5, 'Shah Pur Kanjriyaan');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `class`
+--
+
+CREATE TABLE `class` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `class`
+--
+
+INSERT INTO `class` (`id`, `name`) VALUES
+(1, 'Class V'),
+(2, 'Class VI'),
+(3, 'Class VII'),
+(4, 'Class VIII'),
+(5, 'Class IX'),
+(6, 'Class X');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `subject`
+--
+
+INSERT INTO `subject` (`id`, `name`) VALUES
+(1, 'Physics'),
+(2, 'Chemistry'),
+(3, 'Biology'),
+(4, 'Computer Science'),
+(5, 'Psychology');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor`
+--
+
+CREATE TABLE `tutor` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `fname` varchar(32) NOT NULL,
+  `cnic` varchar(13) NOT NULL,
+  `gender` tinyint(1) NOT NULL,
+  `cnic_pic_path_1` varchar(32) NOT NULL,
+  `cnic_pic_path_2` varchar(32) NOT NULL,
+  `city` varchar(32) NOT NULL,
+  `phone` varchar(14) NOT NULL,
+  `alt_phone` varchar(14) NOT NULL,
+  `mailing_address` varchar(200) NOT NULL,
+  `permanent_address` varchar(200) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `profile_pic_path` varchar(32) NOT NULL,
+  `teaching_exprerience` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor_documents`
+--
+
+CREATE TABLE `tutor_documents` (
+  `id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL,
+  `document_name` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor_preffered_areas`
+--
+
+CREATE TABLE `tutor_preffered_areas` (
+  `id` int(11) NOT NULL,
+  `area_id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor_preffered_classes`
+--
+
+CREATE TABLE `tutor_preffered_classes` (
+  `id` int(11) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutor_preffered_subjects`
+--
+
+CREATE TABLE `tutor_preffered_subjects` (
+  `id` int(11) NOT NULL,
+  `subject_id` int(11) NOT NULL,
+  `tutor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -53,6 +193,54 @@ ALTER TABLE `administrator`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `area`
+--
+ALTER TABLE `area`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `class`
+--
+ALTER TABLE `class`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `subject`
+--
+ALTER TABLE `subject`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tutor`
+--
+ALTER TABLE `tutor`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tutor_documents`
+--
+ALTER TABLE `tutor_documents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tutor_preffered_areas`
+--
+ALTER TABLE `tutor_preffered_areas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tutor_preffered_classes`
+--
+ALTER TABLE `tutor_preffered_classes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tutor_preffered_subjects`
+--
+ALTER TABLE `tutor_preffered_subjects`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -61,6 +249,54 @@ ALTER TABLE `administrator`
 --
 ALTER TABLE `administrator`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `area`
+--
+ALTER TABLE `area`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `class`
+--
+ALTER TABLE `class`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `subject`
+--
+ALTER TABLE `subject`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tutor`
+--
+ALTER TABLE `tutor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tutor_documents`
+--
+ALTER TABLE `tutor_documents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tutor_preffered_areas`
+--
+ALTER TABLE `tutor_preffered_areas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tutor_preffered_classes`
+--
+ALTER TABLE `tutor_preffered_classes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tutor_preffered_subjects`
+--
+ALTER TABLE `tutor_preffered_subjects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
