5b00f783a5746cd5ee6d5313ad5880cbeeca0155
azizurrahmaan <azizurrahmaan1997@gmail.com>
2019-04-20 22:23:08 +0500

    sql updated

--- national_tutors.sql
+++ national_tutors.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 17, 2019 at 03:12 PM
+-- Generation Time: Apr 20, 2019 at 07:21 PM
 -- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.4
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -40,7 +40,7 @@ CREATE TABLE `administrator` (
 --
 
 INSERT INTO `administrator` (`id`, `name`, `email`, `password`) VALUES
-(1, 'admin', 'a@a.a', 'abc');
+(1, 'admin', 'a@a.a', '!1Aaasdf');
 
 -- --------------------------------------------------------
 
@@ -62,7 +62,9 @@ INSERT INTO `area` (`id`, `name`) VALUES
 (2, 'Johar Town'),
 (3, 'Bahria Town'),
 (4, 'Gajju Ki Maata'),
-(5, 'Shah Pur Kanjriyaan');
+(5, 'Shah Pur Kanjriyaan'),
+(95, 'Shahdara'),
+(96, 'Jain Mandir');
 
 -- --------------------------------------------------------
 
@@ -117,22 +119,30 @@ INSERT INTO `subject` (`id`, `name`) VALUES
 
 CREATE TABLE `tutor` (
   `id` int(11) NOT NULL,
-  `name` varchar(32) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `fname` varchar(32) NOT NULL,
-  `cnic` varchar(13) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `cnic` char(13) NOT NULL,
   `gender` tinyint(1) NOT NULL,
-  `cnic_pic_path_1` varchar(32) NOT NULL,
-  `cnic_pic_path_2` varchar(32) NOT NULL,
-  `city` varchar(32) NOT NULL,
-  `phone` varchar(14) NOT NULL,
-  `alt_phone` varchar(14) NOT NULL,
-  `mailing_address` varchar(200) NOT NULL,
-  `permanent_address` varchar(200) NOT NULL,
-  `password` varchar(32) NOT NULL,
-  `profile_pic_path` varchar(32) NOT NULL,
-  `teaching_exprerience` tinyint(1) NOT NULL
+  `cnic_pic_path_1` varchar(255) NOT NULL,
+  `cnic_pic_path_2` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `phone` char(14) NOT NULL,
+  `alt_phone` char(14) NOT NULL,
+  `mailing_address` varchar(255) NOT NULL,
+  `permanent_address` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `profile_pic_path` varchar(255) NOT NULL,
+  `teaching_experience` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tutor`
+--
+
+INSERT INTO `tutor` (`id`, `name`, `email`, `fname`, `cnic`, `gender`, `cnic_pic_path_1`, `cnic_pic_path_2`, `city`, `phone`, `alt_phone`, `mailing_address`, `permanent_address`, `password`, `profile_pic_path`, `teaching_experience`) VALUES
+(1, 'Arslan Ahmad', 'de.arslanahmad@gmail.com', 'Abbu g', '1234567891230', 1, '155577314911824688495cbb36ddc6e70.jpg', '1555773149339387015cbb36ddca16f.jpg', 'Farooqa(ok, that\'s not a city)', '123254657898', '123254657898', 'bhs shsb bfs gsdbvdsa jfsbzd jsfd.s kj bfhdsfds js', 'xchb ssd,hvbs ufsdbds sdbsd usdjz nsghsdkg nsufds g', '!1Aaasdf', '15556409774151926305cb932916a740.jpg', '1 (no experience)'),
+(2, 'Arslan Ahmad', 'de.arslanahmad@gmail.comyt', 'Abbu g', '1234567891230', 1, '', '', 'Farooqa(ok, that\'s not a city)', '123254657898', '123254657898', 'bdfg sifsybg ifgsui bifgsbgf sursdgh urbgrs', 'zd khbdbsk usdkjbvsrb isdbgu rshggrs hgd r', '!1Aaasdf', '15556436403732666175cb93cf8e81e8.jpg', '1 (no experience)');
 
 -- --------------------------------------------------------
 
@@ -158,6 +168,14 @@ CREATE TABLE `tutor_preffered_areas` (
   `tutor_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tutor_preffered_areas`
+--
+
+INSERT INTO `tutor_preffered_areas` (`id`, `area_id`, `tutor_id`) VALUES
+(1, 3, 1),
+(2, 95, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -170,6 +188,14 @@ CREATE TABLE `tutor_preffered_classes` (
   `tutor_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tutor_preffered_classes`
+--
+
+INSERT INTO `tutor_preffered_classes` (`id`, `class_id`, `tutor_id`) VALUES
+(1, 2, 1),
+(2, 3, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -181,6 +207,14 @@ CREATE TABLE `tutor_preffered_subjects` (
   `subject_id` int(11) NOT NULL,
   `tutor_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tutor_preffered_subjects`
+--
+
+INSERT INTO `tutor_preffered_subjects` (`id`, `subject_id`, `tutor_id`) VALUES
+(1, 2, 1),
+(2, 3, 1);
 
 --
 -- Indexes for dumped tables
@@ -254,7 +288,7 @@ ALTER TABLE `administrator`
 -- AUTO_INCREMENT for table `area`
 --
 ALTER TABLE `area`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
 -- AUTO_INCREMENT for table `class`
@@ -272,7 +306,7 @@ ALTER TABLE `subject`
 -- AUTO_INCREMENT for table `tutor`
 --
 ALTER TABLE `tutor`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tutor_documents`
@@ -284,19 +318,19 @@ ALTER TABLE `tutor_documents`
 -- AUTO_INCREMENT for table `tutor_preffered_areas`
 --
 ALTER TABLE `tutor_preffered_areas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tutor_preffered_classes`
 --
 ALTER TABLE `tutor_preffered_classes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tutor_preffered_subjects`
 --
 ALTER TABLE `tutor_preffered_subjects`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
